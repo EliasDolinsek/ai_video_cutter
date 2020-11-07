6798013fbc7fabe2c354fccb4b0506276d02bea4
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-11-07 18:30:38 +0100

    Implemented yt_content_fetcher

---
+++ video_research/test.py
@@ -0,0 +1,31 @@
+# Standard PySceneDetect imports:
+from scenedetect import VideoManager
+from scenedetect import SceneManager
+
+# For content-aware scene detection:
+from scenedetect.detectors import ContentDetector
+
+def find_scenes(video_path, threshold=30.0):
+    # Create our video & scene managers, then add the detector.
+    video_manager = VideoManager([video_path])
+    scene_manager = SceneManager()
+    scene_manager.add_detector(
+        ContentDetector(threshold=threshold))
+
+    # Base timestamp at frame 0 (required to obtain the scene list).
+    base_timecode = video_manager.get_base_timecode()
+
+    # Improve processing speed by downscaling before processing.
+    video_manager.set_downscale_factor()
+
+    # Start the video manager and perform the scene detection.
+    video_manager.start()
+    scene_manager.detect_scenes(frame_source=video_manager)
+
+    # Each returned scene is a tuple of the (start, end) timecode.
+    return scene_manager.get_scene_list(base_timecode)
+
+scenes = find_scenes("test.mp4")
+file = open("result.txt", "w")
+file.write(scenes)
+file.close()
