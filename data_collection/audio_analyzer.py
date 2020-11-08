ed10f688c7e130cceb091d16f293d0bccbc7f320
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-11-08 12:42:39 +0100

    Add end timestamp

--- data_collection/audio_analyzer.py
+++ data_collection/audio_analyzer.py
@@ -18,6 +18,12 @@ def calculate_step(frame_rate):
 def calculate_begin_timestamps(frames_count):
     return np.array([[i*STEP_VALUE for i in range(frames_count)]])
 
+def calculate_end_timestamps(frames_count):
+    timestamps = calculate_begin_timestamps(frames_count)
+    for i in range(len(timestamps[0])):
+        timestamps[0, i] += WINDOW_VALUE
+
+    return timestamps
 
 def analyze_audio(file):
     [fs, x] = audioBasicIO.read_audio_file(file)
@@ -29,8 +35,11 @@ def analyze_audio(file):
     result = f.reshape(-1, f.shape[0])
 
     begin_timestamps = calculate_begin_timestamps(f.shape[1])
+    end_timestamps = calculate_end_timestamps(f.shape[1])
+
     df = pd.DataFrame(data=result, columns=f_names)
     df["start_timestamp"] = begin_timestamps[0]
+    df["end_timestamp"] = end_timestamps[0]
     
     df.to_csv("test.csv")
 
